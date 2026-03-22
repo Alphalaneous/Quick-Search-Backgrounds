793bf0d7fb7991166548b396911d2272d3a395d0
Ashton <38200084+Alphalaneous@users.noreply.github.com>
2026-03-22 02:08:08 -0400

    2.2081

--- src/main.cpp
+++ src/main.cpp
@@ -5,83 +5,85 @@ using namespace geode::prelude;
 
 class $modify(MyLevelBrowserLayer, LevelBrowserLayer) {
 
-	bool init(GJSearchObject* p0) {
-		
-		if(!LevelBrowserLayer::init(p0)) return false;
+	struct Fields {
+		CCSprite* m_background;
+		CCSize m_size;
+	};
+
+	bool init(GJSearchObject* searchObj) {
+		if (!LevelBrowserLayer::init(searchObj)) return false;
 
 		std::string bgTexture;
 
-		switch (p0->m_searchType)
-		{
-		case SearchType::Downloaded:
-			bgTexture = "DownloadsBG.png"_spr;
-			break;
-		case SearchType::Awarded:
-			bgTexture = "AwardedBG.png"_spr;
-			break;
-		case SearchType::Followed:
-			bgTexture = "FollowedBG.png"_spr;
-			break;
-		case SearchType::Friends:
-			bgTexture = "FriendsBG.png"_spr;
-			break;
-		case SearchType::MostLiked:
-			bgTexture = "LikesBG.png"_spr;
-			break;
-		case SearchType::Magic:
-			bgTexture = "MagicBG.png"_spr;
-			break;
-		case SearchType::Recent:
-			bgTexture = "RecentBG.png"_spr;
-			break;
-		case SearchType::Sent:
-			bgTexture = "SentBG.png"_spr;
-			break;
-		case SearchType::Trending:
-			bgTexture = "TrendingBG.png"_spr;
-			break;
-		default:
-			break;
+		switch (searchObj->m_searchType) {
+			case SearchType::Downloaded:
+				bgTexture = "DownloadsBG.png"_spr;
+				break;
+			case SearchType::Awarded:
+				bgTexture = "AwardedBG.png"_spr;
+				break;
+			case SearchType::Followed:
+				bgTexture = "FollowedBG.png"_spr;
+				break;
+			case SearchType::Friends:
+				bgTexture = "FriendsBG.png"_spr;
+				break;
+			case SearchType::MostLiked:
+				bgTexture = "LikesBG.png"_spr;
+				break;
+			case SearchType::Magic:
+				bgTexture = "MagicBG.png"_spr;
+				break;
+			case SearchType::Recent:
+				bgTexture = "RecentBG.png"_spr;
+				break;
+			case SearchType::Sent:
+				bgTexture = "SentBG.png"_spr;
+				break;
+			case SearchType::Trending:
+				bgTexture = "TrendingBG.png"_spr;
+				break;
+			default:
+				break;
 		}
 
-		if (!bgTexture.empty()) {
-			CCSprite* bg = CCSprite::create(bgTexture.c_str());
-			bg->setUserObject("width", CCFloat::create(bg->getContentSize().width));
-			bg->setUserObject("height", CCFloat::create(bg->getContentSize().height));
-			bg->setID("bg-overlay"_spr);
-			bg->setScale(0.8f);
-			bg->setOpacity(25);
-			bg->setBlendFunc({ GL_ONE, GL_ONE_MINUS_CONSTANT_ALPHA });
-			ccTexParams params = {GL_LINEAR, GL_LINEAR, GL_REPEAT, GL_REPEAT};
-			bg->getTexture()->setTexParameters(&params);
-			
-			bg->setZOrder(-1);
-			bg->setAnchorPoint({0, 0});
-			bg->setRotation(10);
-			CCSize winSize = CCDirector::get()->getWinSize();
 
-			bg->setPosition({-winSize.width/2, -winSize.height/2});
+		if (bgTexture.empty()) return true;
 
-			auto rect = bg->getTextureRect();
-			rect.size = rect.size * (CCPoint(winSize) / CCPoint(bg->getScaledContentSize()) * 2);
-			rect.origin = CCPoint{0, 0};
-			bg->setTextureRect(rect);
-			addChild(bg);
+		auto fields = m_fields.self();
 
-			animateBG(0);
-			schedule(schedule_selector(MyLevelBrowserLayer::animateBG));
-		}
+		fields->m_background = CCSprite::create(bgTexture.c_str());
+		fields->m_size = fields->m_background->getContentSize();
+		fields->m_background->setID("bg-overlay"_spr);
+		fields->m_background->setScale(0.8f);
+		fields->m_background->setOpacity(25);
+		fields->m_background->setBlendFunc({ GL_ONE, GL_ONE_MINUS_CONSTANT_ALPHA });
+		ccTexParams params = {GL_LINEAR, GL_LINEAR, GL_REPEAT, GL_REPEAT};
+		fields->m_background->getTexture()->setTexParameters(&params);
+		
+		fields->m_background->setZOrder(-1);
+		fields->m_background->setAnchorPoint({0, 0});
+		fields->m_background->setRotation(10);
+		auto winSize = CCDirector::get()->getWinSize();
+
+		fields->m_background->setPosition({-winSize.width/2, -winSize.height/2});
+
+		auto rect = fields->m_background->getTextureRect();
+		rect.size = rect.size * (CCPoint(winSize) / CCPoint(fields->m_background->getScaledContentSize()) * 2);
+		rect.origin = CCPoint{0, 0};
+		fields->m_background->setTextureRect(rect);
+		addChild(fields->m_background);
+
+		animateBG(0);
+		schedule(schedule_selector(MyLevelBrowserLayer::animateBG));
+		
 		return true;
 	}
 
 	void animateBG(float dt){
+		auto fields = m_fields.self();
 
-		CCSprite* bg = typeinfo_cast<CCSprite*>(getChildByID("bg-overlay"_spr));
-
-		auto width = static_cast<CCFloat*>(bg->getUserObject("width"))->getValue();
-		auto height = static_cast<CCFloat*>(bg->getUserObject("height"))->getValue();
-
-		auto rect = bg->getTextureRect();
+		auto rect = fields->m_background->getTextureRect();
 
 		static float dX;
 		static float dY;
@@ -92,16 +94,16 @@ class $modify(MyLevelBrowserLayer, LevelBrowserLayer) {
 		dX = rect.origin.x - 2 * dt;
 		dY = rect.origin.y + 6 * dt;
 
-		if(dX >= std::abs(width)) {
+		if (dX >= std::abs(fields->m_size.width)) {
 			dX = 0;
 		}
 
-		if(dY >= std::abs(height)) {
+		if (dY >= std::abs(fields->m_size.height)) {
 			dY = 0;
 		}
 
 		rect.origin = CCPoint{dX, dY};
 
-        bg->setTextureRect(rect);
+        fields->m_background->setTextureRect(rect);
 	}
 };
